cc9a8c40176d804ab11064c9b4c9564d8e176233
unknown <tcsadityasalvi>
2021-08-11 05:45:52 +0000

    abstraction, inheritance

---
+++ src/com/tcs/final1/keyword/Ticket.java
@@ -0,0 +1,15 @@
+package com.tcs.final1.keyword;
+
+public final class Ticket {
+	private final String movie = "Interstellar";
+	private float price;
+	
+	public final void sell() {
+		
+	}
+public final void sell(float price) {
+		
+	}
+	
+
+}
