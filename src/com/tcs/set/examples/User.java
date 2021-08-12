556d5496a4d26e6a8c91eb674863a3eaf76b5c51
unknown <tcsadityasalvi>
2021-08-12 07:22:47 +0000

    tree set

--- src/com/tcs/set/examples/User.java
+++ src/com/tcs/set/examples/User.java
@@ -1,5 +1,30 @@
 package com.tcs.set.examples;
 
-public class User {
+import java.util.Comparator;
+
+public class User implements Comparable<User> {
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return Integer.toString(age);
+	}
+	@Override
+	public int compareTo(User user) {
+		return user.getAge() - this.getAge();
+	}
 
 }
