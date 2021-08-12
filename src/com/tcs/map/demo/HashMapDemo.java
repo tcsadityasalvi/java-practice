028315d27f079cabbf956e36ce7c633626039013
unknown <tcsadityasalvi>
2021-08-12 08:56:31 +0000

    Map demo

--- src/com/tcs/map/demo/HashMapDemo.java
+++ src/com/tcs/map/demo/HashMapDemo.java
@@ -3,17 +3,21 @@ package com.tcs.map.demo;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
 
 public class HashMapDemo {
 	public static void main(String[] args) {
-		createHashMap();
+	//	createHashMap();
 	}
 	
-	private static void createHashMap() {
-		Map<String, Float> mathScores = new HashMap<String, Float>();
+	private static void treeMap() {
+		Map<String, Float> mathScores = new TreeMap<String, Float>();
 		for (int i = 0; i < 20; i++) {
 			mathScores.put("Student" +i, new Float(100 * Math.random() ));
 		}
+		boolean exits = mathScores.containsKey("Student1");
+		System.out.println(exits);
+		mathScores.put("student1", 34F);
 		System.out.println(mathScores);
 	}
 
