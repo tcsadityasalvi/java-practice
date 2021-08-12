0bca021f7dbff39d5a38128626b374eab23630a4
unknown <tcsadityasalvi>
2021-08-12 06:18:42 +0000

    Filtered

--- src/com/tcs/listdemo/ArraylistDemo.java
+++ src/com/tcs/listdemo/ArraylistDemo.java
@@ -3,34 +3,50 @@ package com.tcs.listdemo;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Comparator;
-
-
+import java.util.stream.Collectors;
 
 public class ArraylistDemo {
 	public static void main(String[] args) {
 		List<Integer> numbers = createList();
+		//sortList(numbers);
+		filterList(numbers);
+		
+	}
+
+	public static void filterList(List<Integer> numbers) {
+		numbers.forEach(number ->{
+			System.out.println(number % 2 == 0);
+		});
+		List<Integer> filtered = numbers.stream()
+				.filter((number)-> number % 2 ==0)
+				.collect(Collectors.toList());
+				System.out.println(filtered);
+
+	}
+
+	private static void sortList(List<Integer> numbers) {
 		System.out.println(numbers);
 		numbers.sort(new Comparator<Integer>() {
 
 			@Override
 			public int compare(Integer o1, Integer o2) {
 				// TODO Auto-generated method stub
-				return o2-o1;
+				return o2 - o1;
 			}
 		});
-		System.out.println("after sorting"+numbers);
-		
+		System.out.println("after sorting" + numbers);
 	}
+
 	private static List<Integer> createList() {
 		List<Integer> numbers = new ArrayList<Integer>();
 		numbers.add(1);
-		//System.out.println(numbers.get(0));
-		
-		 for (int count=0; count<20; count++) {
-			 numbers.add(count);
-		 }
+		// System.out.println(numbers.get(0));
+
+		for (int count = 0; count < 20; count++) {
+			numbers.add(count);
+		}
 		return numbers;
-		
+
 	}
 
 }
